1f4a0ad300ceba040c8e5cd8891649e6fba1097d
unknown <you@example.com>
2021-12-21 11:27:22 +0900

    스프링부트 세팅

---
+++ src/test/java/riot/riot/RiotApplicationTests.java
@@ -0,0 +1,13 @@
+package riot.riot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
